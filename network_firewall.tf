14c3f475683e254e219dc8cda3944efb9281bcfd
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-09-12 19:50:33 +0100

    create a firewall for each subnet

--- network_firewall.tf
+++ network_firewall.tf
@@ -1,14 +1,16 @@
 resource "aws_networkfirewall_firewall" "main" {
+  count               = 3
   name                = local.name-prefix
   firewall_policy_arn = aws_networkfirewall_firewall_policy.main.arn
   vpc_id              = aws_vpc.main.id
   subnet_mapping {
-    subnet_id = aws_subnet.firewall.id
+    subnet_id = aws_subnet.firewall[count.index].id
   }
 }
 
 resource "aws_networkfirewall_firewall_policy" "main" {
-  name = "main"
+  count = 3
+  name  = "main"
 
   firewall_policy {
     # policy_variables {
@@ -23,7 +25,7 @@ resource "aws_networkfirewall_firewall_policy" "main" {
     stateless_fragment_default_actions = ["aws:drop"]
     stateless_rule_group_reference {
       priority     = 1
-      resource_arn = aws_networkfirewall_rule_group.main.arn
+      resource_arn = aws_networkfirewall_rule_group.main[count.index].arn
     }
   }
 }
