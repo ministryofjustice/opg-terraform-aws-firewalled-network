81cd64d83fc9d95aad3cd58b3dd5997909da816e
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-09-15 16:08:00 +0100

    set default action to drop

--- network_firewall.tf
+++ network_firewall.tf
@@ -36,7 +36,7 @@ resource "aws_networkfirewall_firewall_policy" "example" {
   name = "example"
 
   firewall_policy {
-    stateless_default_actions          = ["aws:pass"]
+    stateless_default_actions          = ["aws:drop"]
     stateless_fragment_default_actions = ["aws:drop"]
     stateless_rule_group_reference {
       priority     = 1
