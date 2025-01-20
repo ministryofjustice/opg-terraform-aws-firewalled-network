d349912caae0a030dc1fe505d1dc110d95c43ee5
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-01-20 10:22:04 +0000

    make maps and merge

--- network_firewall.tf
+++ network_firewall.tf
@@ -9,8 +9,8 @@ resource "aws_networkfirewall_firewall" "main" {
 }
 
 locals {
-  rule_file         = try(aws_networkfirewall_rule_group.rule_file[0].arn, {})
-  domain_allow_list = try(aws_networkfirewall_rule_group.domain_allow_list[0].arn, {})
+  rule_file         = try(tomap(aws_networkfirewall_rule_group.rule_file[0].arn), {})
+  domain_allow_list = try(tomap(aws_networkfirewall_rule_group.domain_allow_list[0].arn), {})
   rule_group_arns   = merge(local.rule_file, local.domain_allow_list)
 }
 
