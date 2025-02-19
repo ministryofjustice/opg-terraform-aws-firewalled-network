63b489d8a947f355cfaabd39046a2cad92e9843d
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-02-19 13:45:26 +0000

    pull firewall policy out so that user can manage which rule-set type they want to use

--- network_firewall.tf
+++ network_firewall.tf
@@ -1,72 +1,10 @@
 resource "aws_networkfirewall_firewall" "main" {
   count               = 3
   name                = "${local.name-prefix}-${data.aws_availability_zones.all.names[count.index]}"
-  firewall_policy_arn = aws_networkfirewall_firewall_policy.main.arn
+  firewall_policy_arn = var.aws_networkfirewall_firewall_policy.arn
   vpc_id              = aws_vpc.main.id
   subnet_mapping {
     subnet_id = aws_subnet.firewall[count.index].id
-  }
-}
-
-locals {
-  rule_file         = try(aws_networkfirewall_rule_group.rule_file[0].arn, "")
-  domain_allow_list = try(aws_networkfirewall_rule_group.domain_allow_list[0].arn, "")
-  rule_group_arns   = toset(sort(concat([local.rule_file], [local.domain_allow_list])))
-}
-
-resource "aws_networkfirewall_firewall_policy" "main" {
-  name = "main"
-
-  firewall_policy {
-    stateless_default_actions          = ["aws:forward_to_sfe"]
-    stateless_fragment_default_actions = ["aws:forward_to_sfe"]
-
-    stateful_engine_options {
-      rule_order              = "DEFAULT_ACTION_ORDER"
-      stream_exception_policy = "DROP"
-    }
-    dynamic "stateful_rule_group_reference" {
-      for_each = local.rule_group_arns
-      content {
-        resource_arn = stateful_rule_group_reference.key
-      }
-    }
-  }
-}
-
-resource "aws_networkfirewall_rule_group" "rule_file" {
-  count    = var.network_firewall_rules_file == "" ? 0 : 1
-  capacity = 100
-  name     = "rule-file-${replace(filebase64sha256(var.network_firewall_rules_file), "/[^[:alnum:]]/", "")}"
-  type     = "STATEFUL"
-  rules    = file(var.network_firewall_rules_file)
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_networkfirewall_rule_group" "domain_allow_list" {
-  count    = length(var.domain_allow_list) == 0 ? 0 : 1
-  capacity = 100
-  name     = "domain-allow-list${replace(sha256(jsonencode(var.domain_allow_list)), "/[^[:alnum:]]/", "")}"
-  type     = "STATEFUL"
-  rule_group {
-    stateful_rule_options {
-      rule_order = "DEFAULT_ACTION_ORDER"
-    }
-    rules_source {
-      rules_source_list {
-        generated_rules_type = "ALLOWLIST"
-        target_types = [
-          "HTTP_HOST",
-          "TLS_SNI"
-        ]
-        targets = var.domain_allow_list
-      }
-    }
-  }
-  lifecycle {
-    create_before_destroy = true
   }
 }
 
